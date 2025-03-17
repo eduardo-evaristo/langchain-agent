12261c01dc3dad3f1218ed728431c0f524292446
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-03-16 21:43:32 -0300

    fix: pass handle_parsing_errors=True to the AgentExecutor to fix parsing errors

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 from browse_tool import browse_text
 from browse_images_tool import browse_images
+from browse_news_tool import browse_news
 from llm import llm
 from langchain.agents import initialize_agent
 from langchain_core.messages import HumanMessage, SystemMessage
@@ -7,18 +8,22 @@ import sys
 
 messages = [
     SystemMessage(
-        'Responda em português brasileiro, seja cordial, porém assertiva e concisa. Não use símbolos como: *, #, " em suas respostas. Quando o usuário pedir-te uma imagem, responda o link da que parecer-te mais pertinente'
+        'Responda em português brasileiro, seja cordial, porém assertiva e concisa. Não use símbolos como: *, #, " em suas respostas. Quando o usuário pedir-te uma imagem, responda o link da que parecer-te mais pertinente.'
     ),
     HumanMessage(sys.argv[1]),
 ]
 
 # Tools to be used by the agent
-tools = [browse_text, browse_images]
+tools = [browse_text, browse_images, browse_news]
 
 # Load the agent with its LLM and its available tools
 # verbose needs to be set to True
 agent = initialize_agent(
-    llm=llm, tools=tools, agent="zero-shot-react-description", verbose=True
+    llm=llm,
+    tools=tools,
+    agent="zero-shot-react-description",
+    verbose=True,
+    handle_parsing_errors=True,
 )
 
 # agent.run is deprecated
